0b7837ddbe025e6219551699a58a412a2d2da1f1
Fernanda Neves <fernanda_neves@ufms.br>
2026-04-22 14:50:10 -0400

    implementação do método de jacobi no sequencial.cpp, acredito que ficou bastante sucinto mas ficou claro também, está apenas seguindo a fórmula tradicional

---
+++ src/sequencial.cpp
@@ -0,0 +1,50 @@
+#include "jacobi.h"
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+vector<double> jacobi_sequencial(
+    vector<vector<double>>& A,
+    vector<double>& b,
+    int max_iter,
+    double epsilon
+) {
+    int n = A.size();
+
+    vector<double> x_old(n, 0.0);
+    vector<double> x_new(n, 0.0);
+
+    for (int k = 0; k < max_iter; k++) {
+
+        // Calcula nova iteração
+        for (int i = 0; i < n; i++) {
+            double soma = 0.0;
+
+            for (int j = 0; j < n; j++) {
+                if (j != i) {
+                    soma += A[i][j] * x_old[j];
+                }
+            }
+
+            x_new[i] = (b[i] - soma) / A[i][i];
+        }
+
+        // Calcula erro
+        double erro = 0.0;
+        for (int i = 0; i < n; i++) {
+            erro += fabs(x_new[i] - x_old[i]);
+        }
+
+        // Verifica convergência
+        if (erro < epsilon) {
+            cout << "Convergiu em " << k + 1 << " iteracoes.\n";
+            break;
+        }
+
+        // Atualiza vetor
+        x_old = x_new;
+    }
+
+    return x_new;
+}
