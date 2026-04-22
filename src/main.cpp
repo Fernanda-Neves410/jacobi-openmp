9347f74b245b32271ee6f8f207e4e0c0ad97dce3
kaue14 <87403692+kaue14@users.noreply.github.com>
2026-04-22 19:34:13 -0400

    Enhance main.cpp with execution time measurement
    Added timing for Jacobi parallel execution and improved output.

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@ int main() {
     double epsilon = 1e-6;
     vector<int> num_threads_teste = {1, 2, 4, 6, 8, 12};
     int max_iter = 1000;
+    int i, j;
 
     // alterar tamanho da matriz
     int n = 5;
@@ -19,23 +20,27 @@ int main() {
     vector<double> b(n, 0.0);
     srand(time(NULL)); 
     
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
             A[i][j] = (rand() % 10) + 1;
         }
         b[i] = (rand() % 50) + 1; 
     }
     
     //  printa num de threads e valores de x
+    //  printa tempo
     for(int threads : num_threads_teste) {
+        double temp_inicio = omp_get_wtime();
         auto resultado = jacobi_paralelo(A, b, max_iter, epsilon, threads);
+        double temp_final = omp_get_wtime();
+        double tempo_total = temp_final - temp_inicio;
 
-        cout << "Threads: " << threads << endl;
 
-    for(int i = 0; i < n; i++){
-        cout << "x[" << i << "]=" << resultado[i] << "  ";
-    }
-        cout << endl;
+        cout << "Threads: " << threads;
+        cout << "   Tempo: " << tempo_total << "s" << "\n";
+        for(i = 0; i < n; i++)
+            cout << "x[" << i << "]=" << resultado[i] << "  ";
+        cout << "\n\n";
     }
     
 
