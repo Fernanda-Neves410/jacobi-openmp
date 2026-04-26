d12173e209879c3fb40ad74678b55143a04a2e4e
kaue14 <87403692+kaue14@users.noreply.github.com>
2026-04-26 04:46:49 -0400

    Refactor jacobi_paralelo for better performance
    Refactor jacobi_paralelo function for improved convergence checking and error handling.

--- src/paralelo.cpp
+++ src/paralelo.cpp
@@ -2,33 +2,44 @@
 #include <omp.h>
 #include <cmath>
 #include <vector>
+#include <iostream>
 
 vector<double> jacobi_paralelo(vector<vector<double>>& A, vector<double>& b, int max_iter, double epsilon, int num_threads) {
     int n = b.size();
     vector<double> x_old(n, 0.0);
     vector<double> x_new(n, 0.0);
-    double max_diff = 0.0;
-    int iter = 0;
+    double erro_max;
+    int k;
+    
     omp_set_num_threads(num_threads);
 
-    do {
-        #pragma omp parallel for reduction(max:max_diff) shared(A, b, x_old, x_new, n) schedule(static) default(none)
+    for (k = 0; k < max_iter; k++) {
+        erro_max = 0.0;
+
+        #pragma omp parallel for reduction(max:erro_max) shared(A, b, x_old, x_new, n) schedule(static) default(none)
         for (int i = 0; i < n; i++) {
             double soma = 0.0;
             for (int j = 0; j < n; j++) {
-                if (i != j)
-                 soma += A[i][j] * x_old[j];
+                if (i != j) {
+                    soma += A[i][j] * x_old[j];
+                }
             }
+            
             x_new[i] = (b[i] - soma) / A[i][i];
 
-            double diff = std::abs(x_new[i] - x_old[i]);
-            if (diff > max_diff) 
-                max_diff = diff;
+            double erro = fabs(x_new[i] - x_old[i]);
+            if (erro > erro_max) {
+                erro_max = erro;
+            }
         }
 
+        if (erro_max < epsilon) {
+            cout << "Convergiu em " << k + 1 << " iteracoes.\n";
+            break; 
+        }
+        
         x_old = x_new;
-        iter++;
-    } while (max_diff > epsilon && iter < max_iter);
+    }
 
     return x_new;
 }
